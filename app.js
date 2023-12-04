99d381a87acf7edc6f0e14c70354493a39fa23b7
PrakashKumaraguru <prakash08122001@gmail.com>
2023-12-04 20:24:53 +0530

    basic hello world display in port 8090

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const exhbs = require('express-handlebars');
-const bodyparser = require('bodyparser');
-const { log } = require('console');
+const bodyparser = require('body-parser');
 
-app.engine('hbs', exhbs.engine());
+
+app.engine('hbs', exhbs.engine({layoutsDir:'views/',defaultLayout:'main',extname:'hbs'}));
 app.set('view engine','hbs');
 app.set('views','views');
 
-app.set('/', (req,res)=>{
+
+app.get('/', (req,res)=>{
     let message = 'Hello World';
+    res.render('main', {message})
 })
 app.listen(8090,()=>{
-    log('listening on port 8090');
+    console.log('listening on port 8090');
 })
