20aeba4011dd20a14994ff0c6bb5330c667ee954
PrakashKumaraguru <prakash08122001@gmail.com>
2023-12-04 18:34:51 +0530

    express head set

--- mongocurd/app.js
+++ mongocurd/app.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const express = require('express');
+const app = express();
+const exhbs = require('express-handlebars');
+const bodyparser = require('bodyparser');
+
+app.engine('hbs', exhbs.engine());
+app.set('view engine','hbs');
+app.set('views','views');
+
