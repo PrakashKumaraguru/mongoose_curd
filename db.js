a1aabd3922576f0b2f6bf7aa17ec5112df519979
PrakashKumaraguru <prakash08122001@gmail.com>
2023-12-04 20:44:57 +0530

    create db.js for create database connection

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongodb = require('mongodb')
+const Mongoclient = mongodb.MongoClient;
+
+let database;
+async function getDataBase(){
+    const client = await Mongoclient.connect('mongodb://127.0.0.1:27017/');
+
+    database = client.db('library')
+    if (!database) {
+        console.log('database is not available ');
+    }else{
+        return database;
+    }
+
+}
+module.exports = {
+    getDataBase
+}
