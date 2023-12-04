d25bc5955c61427030a2f33e83f94ee7f29b74d0
PrakashKumaraguru <prakash08122001@gmail.com>
2023-12-04 19:57:14 +0530

    secomd commit folder alter

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const exhbs = require('express-handlebars');
 const bodyparser = require('bodyparser');
+const { log } = require('console');
 
 app.engine('hbs', exhbs.engine());
 app.set('view engine','hbs');
 app.set('views','views');
 
+app.set('/', (req,res)=>{
+    let message = 'Hello World';
+})
+app.listen(8090,()=>{
+    log('listening on port 8090');
+})
