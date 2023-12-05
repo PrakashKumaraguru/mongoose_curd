1312bdf98dfdd5402f027af491daad27b28e2b47
PrakashKumaraguru <prakash08122001@gmail.com>
2023-12-05 11:22:55 +0530

    insert data and redirect to '/'

--- app.js
+++ app.js
@@ -2,22 +2,39 @@ const express = require('express');
 const app = express();
 const exhbs = require('express-handlebars');
 const bodyparser = require('body-parser');
-const dob = require('./db.js')
+const dbs = require('./db.js');
+const { Collection } = require('mongodb');
 
 
 app.engine('hbs', exhbs.engine({layoutsDir:'views/',defaultLayout:'main',extname:'hbs'}));
 app.set('view engine','hbs');
 app.set('views','views');
+app.use(bodyparser.urlencoded({extended:true}));
 
 
 app.get('/', async(req,res)=>{
-    let database = await dob.getDataBase();
+    let database = await dbs.getDataBase();
     const collection = database.collection('books');
     const curser = collection.find({});
     let employee  = await curser.toArray();
 
-    let message = 'Hello World';
-    res.render('main', {message,employee})
+    let message = '';
+    switch (req.query.status) {
+        case '1':
+            message = 'data is inserted ';
+            break;
+    
+        default:
+            break;
+    }
+    res.render('main', {message,employee});
+})
+app.post('/stored-book',async(req,res) => {
+    let database = await dbs.getDataBase();
+    const collection = database.collection('books');
+    let book = {title:req.body.title,author:req.body.author};
+    await collection.insertOne(book);
+    return res.redirect('/?status=1');
 })
 
 app.listen(8090,()=>{
